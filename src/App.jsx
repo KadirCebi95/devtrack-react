9bb72cb14dcc55ab2cc2084b7782c1ec7847d76f
Kadir Çebi <kadirjsdevx@gmail.com>
2026-08-07 01:38:24 +0300

    Feat: add developer search and empty state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import ProfileCard from "./components/ProfileCard";
 
 function App() {
+  const [search, setSearch] = useState("");
   const users = [
     {
       id: 1,
@@ -23,16 +25,26 @@ function App() {
       buttonText: "Contact",
     },
   ];
+  const filteredUsers = users.filter((user) => {
+    return user.name.toLowerCase().includes(search.toLowerCase());
+  });
   return (
     <>
       <Header />
+      <input
+        type="text"
+        placeholder="Search Developer..."
+        onChange={(event) => {
+          setSearch(event.target.value);
+        }}
+      />
+
       <div className="card-container">
-        {users.map((user) => (
-          <ProfileCard
-          key={user.id}
-           user= {user}
-          />
-        ))}
+        {filteredUsers.length > 0 ? (
+          filteredUsers.map((user) => <ProfileCard key={user.id} user={user} />)
+        ) : (
+          <p>No developers found.</p>
+        )}
       </div>
     </>
   );
