a98244d301e88d0fc335978c834f38e2cc7f2b46
Kadir Çebi <kadirjsdevx@gmail.com>
2026-08-10 22:17:35 +0300

    Feat:add role filtering and active filter state

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,44 @@ import ProfileCard from "./components/ProfileCard";
 
 function App() {
   const [search, setSearch] = useState("");
+  const [selectedRole, setSelectedRole] = useState("All");
+
   const users = [
     {
       id: 1,
       name: "Kadir",
       role: "Frontend Software Engineer",
       buttonText: "Follow",
-      city:"İstanbul",
+      city: "İstanbul",
     },
     {
       id: 2,
       name: "Ahmet",
       role: "Backend Developer",
       buttonText: "Message",
-      city:"İzmir",
+      city: "İzmir",
     },
     {
       id: 3,
       name: "Ayşe",
       role: "UI/UX Designer",
       buttonText: "Contact",
-      city:"Kars"
+      city: "Kars",
     },
   ];
 
   const normalizedSearch = search.toLowerCase().trim();
   const filteredUsers = users.filter((user) => {
-  return (
-    user.name.toLowerCase().includes(normalizedSearch) ||
-    user.role.toLowerCase().includes(normalizedSearch) || 
-    user.city.toLowerCase().includes(normalizedSearch)
-  );
-});
+    const searchMatches =
+      user.name.toLowerCase().includes(normalizedSearch) ||
+      user.role.toLowerCase().includes(normalizedSearch) ||
+      user.city?.toLowerCase().includes(normalizedSearch);
+
+    const roleMatches =
+      selectedRole === "All" || user.role.includes(selectedRole);
+
+    return searchMatches && roleMatches;
+  });
   return (
     <>
       <Header />
@@ -47,10 +53,40 @@ function App() {
           setSearch(event.target.value);
         }}
       />
+      <div className="role-filters">
+        <button
+          className={selectedRole === "All" ? "active" : ""}
+          onClick={() => setSelectedRole("All")}
+        >
+          All
+        </button>
+
+        <button
+          className={selectedRole === "Frontend" ? "active" : ""}
+          onClick={() => setSelectedRole("Frontend")}
+        >
+          Frontend
+        </button>
+
+        <button
+          className={selectedRole === "Backend" ? "active" : ""}
+          onClick={() => setSelectedRole("Backend")}
+        >
+          Backend
+        </button>
+
+        <button
+          className={selectedRole === "UI/UX" ? "active" : ""}
+          onClick={() => setSelectedRole("UI/UX")}
+        >
+          UI/UX
+        </button>
+        <p>{selectedRole}</p>
+      </div>
 
       <div className="card-container">
         {filteredUsers.length > 0 ? (
-          filteredUsers.map((user) => <ProfileCard key={user.id} user={user}  />)
+          filteredUsers.map((user) => <ProfileCard key={user.id} user={user} />)
         ) : (
           <p>No developers found.</p>
         )}
